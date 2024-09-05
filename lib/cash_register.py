cf4263d3a629f0e6a1369f589f0bf8c82d6059d3
Keith Murimi <keith.murimi@student.moringaschool.com>
2024-09-05 13:27:16 +0300

    Passed 5 to complete later

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+  def __init__(self, discount = 0):
+    self.total = 0
+    self.discount = discount
+    self.items = []
+
+  def add_item(self, item, price, quantity = 0):
+    self.items.append(item)
+    self.total += price * quantity
+    return self.total
+
+  def get_total(self):
+    return self.total
+
+  def clear(self):
+    self.total = 0
+    self.discount = 0
+    self.items = []
+
+
+# add_item = CashRegister()
+# add_item.add_item(1.99)
+# add_item.add_item(1.99)
+  # pass
