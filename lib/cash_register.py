899321a4f32467e5f4660dbc859f0e9a46d9c067
Keith Murimi <keith.murimi@student.moringaschool.com>
2024-09-07 16:43:53 +0300

    Passed 5 continue later

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -6,21 +6,40 @@ class CashRegister:
     self.discount = discount
     self.items = []
 
-  def add_item(self, item, price, quantity = 0):
-    self.items.append(item)
+  def add_item(self, item, price, quantity = 1):
+    self.items.append((item, price, quantity))
     self.total += price * quantity
     return self.total
+  
+  def apply_discount(self):
+        if self.discount > 0:
+            discount_amount = self.total * (self.discount / 100)
+            self.total -= discount_amount
 
   def get_total(self):
     return self.total
+  
+  def get_items(self):
+    return self.items
+
+  def subtract_last_item(self):
+        if self.items:
+            last_item = self.items.pop()
+            item_name, price, quantity = last_item
+            self.total -= price * quantity
+        return self.total
 
   def clear(self):
     self.total = 0
     self.discount = 0
     self.items = []
+  # pass
 
 
+register = CashRegister(discount=10)
+register.add_item("eggs", 1.99, 2)
+register.add_item("milk", 2.99, 1)
+print("Total:", register.get_total())
 # add_item = CashRegister()
 # add_item.add_item(1.99)
 # add_item.add_item(1.99)
-  # pass
